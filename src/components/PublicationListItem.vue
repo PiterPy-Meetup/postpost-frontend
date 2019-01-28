4da97fc1b711934ded7f58c8f9aa3cf7d4405fbd
Sergey Sokolov <dmemory7@ya.ru>
2019-01-29 00:44:23 +0300

    Fix linter's warnings and errors

--- src/components/PublicationListItem.vue
+++ src/components/PublicationListItem.vue
@@ -1,29 +1,27 @@
 <template>
-    <v-card>
-        <v-card-title primary-title>
-            <div>
-                <div class="headline">Публикация №{{ publication.id }}</div>
-            </div>
-        </v-card-title>
-    </v-card>
+  <v-card>
+    <v-card-title primary-title>
+      <div>
+        <div class='headline'>Публикация №{{ publication.id }}</div>
+      </div>
+    </v-card-title>
+  </v-card>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import Vue from 'vue';
 import { Prop, Component } from 'vue-property-decorator';
-import { PublicationInterface } from '@/interfaces.ts' 
-
+import { PublicationInterface } from '@/interfaces.ts';
 
 @Component
-// почему publication-list-item, а не publication: 
+// почему publication-list-item, а не publication:
 // https://vuejs.org/v2/style-guide/#Multi-word-component-names-essential
 // https://vuejs.org/v2/style-guide/#Tightly-coupled-component-names-strongly-recommended
 export default class PublicationListItem extends Vue {
-    @Prop({type: Object as () => PublicationInterface})
-    publication!: PublicationInterface;
+  @Prop({ type: Object as () => PublicationInterface })
+  public publication!: PublicationInterface;
 }
 </script>
 
 <style>
-
 </style>
