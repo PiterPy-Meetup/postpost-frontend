5df9c4d1b8e60a6bf45c976bbc1d3a1baa074ea0
Sergey Sokolov <dmemory7@ya.ru>
2019-03-09 15:28:25 +0300

    Add Sentry integration

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,17 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import '@mdi/font/css/materialdesignicons.css';
+import * as Sentry from '@sentry/browser';
 
 Vue.config.productionTip = false;
+
+Sentry.init({
+  dsn: 'https://ee58a2f4d06148e59af5473f2e71ff02@sentry.io/1411606',
+  integrations: [new Sentry.Integrations.Vue({
+    Vue,
+    attachProps: true,
+  })],
+});
 
 new Vue({
   router,
